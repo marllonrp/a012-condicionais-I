62b55394053df0f31a78622823e325298a1262b2
marllonrp <marllonrp@yahoo.com.br>
2022-09-06 11:49:00 -0300

    Exercícios diários.

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,21 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+let idade = 26;
+let terminouEnsinoMédio = true;
+let isCursandoOutraFaculdade = true;
 
-if(){
+if (idade >= 18) {
+  console.log("Maior de idade.");
+} else {
+  console.log("Menor de idade.");
+}
 
-}else{
-	
+if (terminouEnsinoMédio) {
+  console.log("Terminou o ensino médio.");
+} else {
+  console.log("Não terminou o ensino médio.");
+}
+
+if (!isCursandoOutraFaculdade) {
+  console.log("Não está cursando ensino superior");
+} else {
+  console.log("Está cursando ensino superior.");
 }
